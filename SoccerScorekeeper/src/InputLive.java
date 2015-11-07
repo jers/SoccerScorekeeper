51615c4c20d9baec26d3c8919e5a59e2c3d5caf8
jers <jeremy@ncf.ca>
2015-11-07 18:13:14 -0500

    nothing to say really

--- SoccerScorekeeper/src/InputLive.java
+++ SoccerScorekeeper/src/InputLive.java
@@ -1,16 +1,22 @@
 
 
-class LiveInput {
+class InputLive {
 
-	LiveInputUI LiveUI;	
-	Match match;
-	ArrayList<Team> TeamList;
+	private LiveInputUI LiveUI;	
+	private Match match;
+	private String teamName;
+	private String playerName;
+	private HashTable<Team, String> TeamList;
+	private Team eTeam;
+	private int selection;
+	private boolean isGoal; 
+	private boolean isRed; 
 
-
-	public LiveInput() {
+	public InputLive() {
 		LiveUI = new LiveUI();
 		match = new Match();
 		TeamList = new ArrayList<Team>();
+		selection = 0; 
 
 	}
 
@@ -24,9 +30,44 @@ class LiveInput {
 
 	}
 
-	public InputGameData() {
-		LiveUI
+	public void getGeneralInfo() {
 
+		Teamname = LiveUI.getTeamName();
+		eTeam = TeamList.get(TeamName);
+		playerName = LiveUI.getPlayerName();
+
+
+	}
+
+	public void getShotInfo() {
+		getGeneralInfo();
+		isGoal = LiveUI.getisGoal();
+
+	}
+
+	public void getInfractionInfo() {
+		getGeneralInfo();
+		getInfractionInfo();
+
+	}
+
+	public InputGameData() {
+
+	
+		while(selection != 0) {
+			selection = LiveUI.getInputType();
+			switch(selection) {
+				case 1: 
+					eTeam.addShot(Teamname, isGoal);		
+					break;
+				case 2:
+					eTeam.addInfraction(playerName, isRed);			
+					break; 
+				
+
+			}
+
+		}
 
 	}
 	
