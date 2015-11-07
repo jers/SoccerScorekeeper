de50c0ab22b451311462763056362c9f72ecb607
jers <jeremy@ncf.ca>
2015-11-07 16:53:14 -0500

    Update editInfo.java
    updated editinfo

---
+++ SoccerScorekeeper/src/editInfo.java
@@ -0,0 +1,38 @@
+
+public class editInfo {
+
+//------------------------
+// MEMBER VARIABLES
+//------------------------
+
+//editInfo Associations
+  private Client client;
+  private Authentication authentication; 
+  private edit_UI; 
+  
+//------------------------
+// CONSTRUCTOR
+//------------------------
+
+ 
+
+  public editInfo(Client aClient, Authentication aAuthentication) {
+
+    client = aClient;
+    authentication = aAuthentication; 
+    edit_UI = new editInfo_UI(); 
+    
+  }
+  
+  public push() {
+  
+  }
+  
+  public pull() {
+  
+  
+  }
+  
+  
+  
+}
