f113740e82d15aeb1f53e961fdd6ec380e6bc20d
jers <jeremy@ncf.ca>
2015-11-07 18:51:13 -0500

    input from user

--- SoccerScorekeeper/src/InputLiveUI.java
+++ SoccerScorekeeper/src/InputLiveUI.java
@@ -1,8 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 class InputLiveUI {
 
 
-	public InputLiveUI {
+	public InputLiveUI () {
 
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
 	}
@@ -12,7 +19,7 @@ class InputLiveUI {
 
 	public String getTeamName() {
 
-
+	System.out.println("Enter Team ");
 
 	}
 
