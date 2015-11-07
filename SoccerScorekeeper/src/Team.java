0e385d09402bec0ec5d3824585b2a1681ef83908
ahmadowais123 <owais.khan2@mail.mcgill.ca>
2015-11-07 17:32:21 -0500

    Update Team.java

--- SoccerScorekeeper/src/Team.java
+++ SoccerScorekeeper/src/Team.java
@@ -1,33 +1,53 @@
-public class Team	{
+import java.util.*;
 
-	private ArrayList<Player> playerList = new ArrayList<Player>();
-	private int nPlayers;
-	private Points; 
-	private nInfractions;
+	public Team () 
+	{
 
-	public Team () {
+	private ArrayList<Player> playerList;
+	private String name;
+	private int points;
 
-
+	public Team (String Name) 
+	{
+		this.name=Name;
+		this.playerList=new ArrayList<Player>();
+		this.points=0;
 	}	
 
-
-	public addPlayer(Player nPlayer) {
+	public void addPlayer(Player p)
+	{
+		this.playerList.add(p);
+		public addPlayer(Player nPlayer) {
 		playerList.add(nPlayer);
-
 	}
 
-	public removePlayer() {
-
-
+	public void removePlayer(Player p)
+	{
+		this.playerList.remove(p);
 	}
-
-	public addShot() {
-
-
+	
+	public void addWin()
+	{
+		this.points+=3;
 	}
-
-	public addInfraction() {
-
-
+	
+	public void addDraw()
+	{
+		this.points+=1;
+	}
+	
+	public int getPoints()
+	{
+		return this.points;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public ArrayList<Player> getPlayer()
+	{
+		return this.playerList;
 	}
 }
