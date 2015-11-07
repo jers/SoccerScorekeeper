f2f930f07712602da068e682037abc4ce55bd78a
Nick Purdie <nickpurdie@wpa146010.wireless.mcgill.ca>
2015-11-07 17:27:59 -0500

    shots

--- SoccerScorekeeper/src/Shot.java
+++ SoccerScorekeeper/src/Shot.java
@@ -1,3 +1,15 @@
+import java.util.Date
+
 public class Shot	{
-	
+	private boolean isGoal;
+	private Time time;
+
+	public boolean shotScored(shot)	{
+		return isGoal;
+	}
+
+	public addshot(boolean scored)	{
+		time = (new Date).getTime()
+		isGoal = scored;
+	}
 }
